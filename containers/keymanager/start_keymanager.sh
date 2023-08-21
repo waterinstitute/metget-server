b7ac6bb6edf3cb2c41576e5595df75d2caca0f21
Zach Cobell <zcobell@gmail.com>
2023-08-21 12:57:27 -0400

    Setting keymanager to start with variable prefix and namespace

--- containers/keymanager/start_keymanager.sh
+++ containers/keymanager/start_keymanager.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-kubectl -n metget exec -it metget-keymanager -- /bin/bash
+if [[ $# -lt 2 ]] ; then
+    echo "[ERROR]: Not enough arguments supplied"
+    echo "Usage:"
+    echo "./start_metget_keymanager.sh [namespace] [prefix]"
+    exit 1
+fi
+
+namespace=$1
+prefix=$2
+
+echo "[INFO]: Starting key manager in namespace: $1 with prefix: $2"
+kubectl -n $1 exec -it $2-keymanager -- /bin/bash
