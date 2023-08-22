e91417f16dd3a5d348e365c76546037ab3d7ea9b
Zach Cobell <zcobell@gmail.com>
2023-08-22 16:11:51 -0400

    Updating database container

--- containers/database/schema.sql
+++ containers/database/schema.sql
@@ -1,4 +1,6 @@
+--
 --Creates tables for the storage of metget's metadata
+--
 CREATE TABLE gfs_ncep(
   id SERIAL PRIMARY KEY, 
   forecastcycle TIMESTAMP NOT NULL, 
@@ -110,8 +112,9 @@ CREATE TABLE wpc_ncep(
   url VARCHAR(256) NOT NULL, 
   accessed TIMESTAMP NOT NULL
 );
-
+--
 --Creates tables for the storage of metget's API access
+--
 CREATE TABLE apikeys(
   id SERIAL PRIMARY KEY, 
   key CHAR(41) NOT NULL, 
@@ -122,8 +125,9 @@ CREATE TABLE apikeys(
   expiration TIMESTAMP,
   permissions JSON
 );
-
+--
 --Creates tables for the storage of metget's build requests and status
+--
 CREATE TYPE metget_status AS ENUM(
   'queued', 'running', 'error', 'completed'
 );
@@ -136,12 +140,13 @@ CREATE TABLE requests(
   last_date TIMESTAMP NOT NULL, 
   api_key VARCHAR(128) NOT NULL, 
   credit_usage BIGINT NOT NULL,
-  source_ip VARCHAR(128) NOT NULL,, 
+  source_ip VARCHAR(128) NOT NULL, 
   input_data JSON NOT NULL,
   message JSON NOT NULL
 );
-
+--
 --Create Brin Indexes on forecastcycle for the various tables
+--
 CREATE INDEX gfs_ncep_forecastcycle_idx ON gfs_ncep USING brin (forecastcycle);
 CREATE INDEX gefs_fcst_forecastcycle_idx ON gefs_fcst USING brin (forecastcycle);
 CREATE INDEX hwrf_forecastcycle_idx ON hwrf USING brin (forecastcycle);
@@ -151,7 +156,8 @@ CREATE INDEX ctcx_forecastcycle_idx ON ctcx USING brin (forecastcycle);
 CREATE INDEX hrrr_ncep_forecastcycle_idx ON hrrr_ncep USING brin (forecastcycle);
 CREATE INDEX hrrr_alaska_ncep_forecastcycle_idx ON hrrr_alaska_ncep USING brin (forecastcycle);
 CREATE INDEX wpc_ncep_forecastcycle_idx ON wpc_ncep USING brin (forecastcycle);
-
----Create Brin Index on basin for nhc_fcst and nhc_btk
+--
+--Create Brin Index on basin for nhc_fcst and nhc_btk
+--
 CREATE INDEX nhc_fcst_basin_idx ON nhc_fcst USING brin (basin);
 CREATE INDEX nhc_btk_basin_idx ON nhc_btk USING brin (basin);
