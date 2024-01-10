df7c81e08348baac08b689d10021e2b09b3239a6
Zach Cobell <zcobell@gmail.com>
2024-01-10 09:41:47 -0500

    Changing the keymanager to be a deployment instead of a pod

--- containers/keymanager/start_keymanager.sh
+++ containers/keymanager/start_keymanager.sh
@@ -9,6 +9,7 @@ fi
 
 namespace=$1
 prefix=$2
+pod_name=$(kubectl get pods -l=app=$2-keymanager -n $1 --no-headers | cut -d" " -f1)
 
-echo "[INFO]: Starting key manager in namespace: $1 with prefix: $2"
-kubectl -n $1 exec -it $2-keymanager -- /bin/bash
+echo "[INFO]: Starting key manager in namespace: $1 with prefix: $2 (pod: $pod_name)"
+kubectl -n $1 exec -it $pod_name -- /bin/bash
