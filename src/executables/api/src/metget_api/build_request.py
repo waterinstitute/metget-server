687f9a99d8c5b66e8155e17fd4c3e214fb4e4450
Zach Cobell <zcobell@gmail.com>
2024-05-28 13:00:40 -0400

    Cleaning up request validation

--- src/executables/api/src/metget_api/build_request.py
+++ src/executables/api/src/metget_api/build_request.py
@@ -27,10 +27,13 @@
 #
 ###################################################################################################
 import logging
-from typing import Union
+from typing import Tuple, Union
 
+from libmetget.build.domain import Domain
 from libmetget.build.input import Input
 from libmetget.database.tables import RequestEnum, RequestTable
+
+log = logging.getLogger(__name__)
 
 
 class BuildRequest:
@@ -90,7 +93,7 @@ class BuildRequest:
         request_status: RequestEnum,
         message: str,
         transmit: bool,
-        credits: int,
+        credit_count: int,
     ) -> None:
         """
         This method is used to add a new request to the database and initiate
@@ -100,7 +103,7 @@ class BuildRequest:
             request_status: The status of the request
             message: The message to be added to the database
             transmit: Whether to transmit the request to the queue
-            credits: The number of credits to be deducted from the user's account
+            credit_count: The number of credits to be deducted from the user's account
 
         Returns:
             None
@@ -109,8 +112,6 @@ class BuildRequest:
         import os
 
         import pika
-
-        log = logging.getLogger(__name__)
 
         if transmit:
             host = os.environ["METGET_RABBITMQ_SERVICE_SERVICE_HOST"]
@@ -134,7 +135,7 @@ class BuildRequest:
             source_ip=self.__source_ip,
             input_data=self.__request_json,
             message=message,
-            credit=credits,
+            credit=credit_count,
         )
 
     @staticmethod
@@ -185,97 +186,191 @@ class BuildRequest:
             dt.append(f["tau"])
         return dt
 
-    def validate(self) -> bool:  # noqa: PLR0911, PLR0912
+    def validate(self) -> bool:
         """
         This method is used to validate the request
+
+        Returns:
+            bool: True if the request is valid, False otherwise
+        """
+
+        # ...Step 1: Check if the input was even parsed correctly
+        if not self.__check_input_obj():
+            return False
+
+        # ...Step 2: Check if the options can be fulfilled
+        is_valid = True
+        for domain in self.__input_obj.domains():
+            is_valid = is_valid and self.__validate_domain(domain)
+        return is_valid
+
+    def __validate_domain(self, domain: Domain) -> bool:
+        """
+        This method is used to validate the domain
+
+        Args:
+            domain: The domain object to validate
+
+        Returns:
+            bool: The updated validity status
+        """
+
+        # Check the ensemble member and update is_valid if it returns false
+        is_domain_valid = self.__check_ensemble_member(domain)
+
+        # Get the tau parameter
+        tau = self.__get_tau_parameter(domain)
+
+        # Generate the lookup for the files that will be used
+        if is_domain_valid:
+            lookup, is_lookup_valid = self.__generate_lookup_obj(domain, tau)
+            is_domain_valid = is_domain_valid and is_lookup_valid
+
+            # Check the synoptic request validity
+            if domain.service() != "nhc":
+                is_domain_valid = (
+                    is_domain_valid
+                    and self.__check_synoptic_request_validity(lookup, tau)
+                )
+
+        return is_domain_valid
+
+    def __check_synoptic_request_validity(self, lookup: list, tau: int) -> bool:
+        """
+        This method is used to check the validity of the synoptic request
+
+        Args:
+            lookup: The lookup object
+            tau: The tau parameter
+
+        Returns:
+            bool: True if the synoptic request is valid, False otherwise
+        """
+
+        is_valid = True
+
+        if len(lookup) < 2:
+            self.__error.append("Not enough data available for the requested options")
+            return False
+
+        n_forecasts = self.__count_forecasts(lookup)
+        # dt = self.__check_time_step(lookup)
+        taus = self.__check_forecast_hours(lookup)
+        start_data_time = lookup[0]["forecasttime"]
+        end_date_time = lookup[-1]["forecasttime"]
+
+        if self.__input_obj.start_date() < start_data_time:
+            self.__error.append(
+                "Start date is before data is available: User Start Date: {:s}, Data Start Date: {:s}".format(
+                    self.__input_obj.start_date().strftime("%Y-%m-%d %H:%M"),
+                    start_data_time.strftime("%Y-%m-%d %H:%M"),
+                )
+            )
+            is_valid = False
+
+        if self.__input_obj.end_date() > end_date_time:
+            self.__error.append(
+                "End date is after data is available. User End Date: {:s}, Data End Date: {:s}".format(
+                    self.__input_obj.end_date().strftime("%Y-%m-%d %H:%M"),
+                    end_date_time.strftime("%Y-%m-%d %H:%M"),
+                )
+            )
+            is_valid = False
+
+        if not self.__input_obj.multiple_forecasts():
+            if n_forecasts > 1 and tau == 0:
+                self.__error.append("Multiple forecasts requested")
+                if self.__input_obj.strict():
+                    is_valid = False
+        elif self.__input_obj.nowcast():
+            has_t_not_zero = any(t != 0 for t in taus)
+            if has_t_not_zero:
+                self.__error.append("Nowcast requested but non-zero tau returned")
+                if self.__input_obj.strict():
+                    is_valid = False
+
+        return is_valid
+
+    def __generate_lookup_obj(self, d, tau) -> Tuple[Union[list, dict], bool]:
+        """
+        This method is used to generate the lookup object for the request and
+        check its validity
+
+        Args:
+            d: The domain object
+            tau: The tau parameter
+
+        Returns:
+            Tuple[Union[list, dict], bool]: The lookup object and a boolean indicating if the lookup is valid
+        """
+        lookup = self.__generate_file_list(
+            d.service(),
+            self.__input_obj.data_type(),
+            self.__input_obj.start_date(),
+            self.__input_obj.end_date(),
+            tau,
+            d.storm_year(),
+            d.storm(),
+            d.basin(),
+            d.advisory(),
+            self.__input_obj.nowcast(),
+            self.__input_obj.multiple_forecasts(),
+            d.ensemble_member(),
+        )
+
+        if not lookup:
+            self.__error.append("No data available for the requested options")
+            return lookup, False
+        return lookup, True
+
+    def __get_tau_parameter(self, d: Domain) -> int:
+        """
+        This method is used to get the tau parameter for the request
+
+        Args:
+            d: The domain object
+
+        Returns:
+            int: The tau parameter
         """
         from libmetget.database.filelist import FilelistBase
 
-        logging.getLogger(__name__)
+        if d.service() != "nhc":
+            tau = FilelistBase.check_tau_parameter(
+                d.tau(), d.service(), self.__input_obj.data_type()
+            )
+        else:
+            tau = d.tau()
+        return tau
 
-        # ...Step 1: Check if the input was even parsed correctly
+    def __check_ensemble_member(self, d: Domain) -> bool:
+        """
+        This method is used to check if the ensemble member is valid
+
+        Args:
+            d: The domain object
+
+        Returns:
+            bool: True if the ensemble member is valid, False otherwise
+        """
+        if d.service() == "gefs-ncep" and not d.ensemble_member():
+            self.__error.append("GEFS-NCEP requires an ensemble member")
+            return False
+        return True
+
+    def __check_input_obj(self) -> bool:
+        """
+        This method is used to check if the input object is valid and
+        save any errors that occur
+
+        Returns:
+            bool: True if the input object is valid, False otherwise
+        """
         if not self.__input_obj.valid():
             self.__error.append("Input data could not be parsed")
             for e in self.__input_obj.error():
                 self.__error.append(e)
             return False
-
-        # ...Step 2: Check if the options can be fulfilled
-        for i in range(self.__input_obj.num_domains()):
-            d = self.__input_obj.domain(i)
-
-            if d.service() == "gefs-ncep" and not d.ensemble_member():
-                self.__error.append("GEFS-NCEP requires an ensemble member")
-                return False
-
-            if d.service() != "nhc":
-                tau = FilelistBase.check_tau_parameter(
-                    d.tau(), d.service(), self.__input_obj.data_type()
-                )
-            else:
-                tau = d.tau()
-
-            lookup = self.__generate_file_list(
-                d.service(),
-                self.__input_obj.data_type(),
-                self.__input_obj.start_date(),
-                self.__input_obj.end_date(),
-                tau,
-                d.storm_year(),
-                d.storm(),
-                d.basin(),
-                d.advisory(),
-                self.__input_obj.nowcast(),
-                self.__input_obj.multiple_forecasts(),
-                d.ensemble_member(),
-            )
-
-            if not lookup:
-                self.__error.append("No data available for the requested options")
-                return False
-
-            if d.service() != "nhc":
-                if len(lookup) < 2:
-                    self.__error.append(
-                        "Not enough data available for the requested options"
-                    )
-                    return False
-
-                n_forecasts = self.__count_forecasts(lookup)
-                # dt = self.__check_time_step(lookup)
-                taus = self.__check_forecast_hours(lookup)
-                start_data_time = lookup[0]["forecasttime"]
-                end_date_time = lookup[-1]["forecasttime"]
-
-                if self.__input_obj.start_date() < start_data_time:
-                    self.__error.append(
-                        "Start date is before data is available: User Start Date: {:s}, Data Start Date: {:s}".format(
-                            self.__input_obj.start_date().strftime("%Y-%m-%d %H:%M"),
-                            start_data_time.strftime("%Y-%m-%d %H:%M"),
-                        )
-                    )
-                    if self.__input_obj.strict():
-                        return False
-
-                if self.__input_obj.end_date() > end_date_time:
-                    self.__error.append("End date is after data is available")
-                    if self.__input_obj.strict():
-                        return False
-
-                if not self.__input_obj.multiple_forecasts():
-                    if n_forecasts > 1 and tau == 0:
-                        self.__error.append("Multiple forecasts requested")
-                        if self.__input_obj.strict():
-                            return False
-                elif self.__input_obj.nowcast():
-                    for t in taus:
-                        if t != 0:
-                            self.__error.append(
-                                "Nowcast requested but non-zero tau returned"
-                            )
-                            if self.__input_obj.strict():
-                                return False
-                            break
         return True
 
     @staticmethod
