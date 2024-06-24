Fixing off-by-one error in the size of the domain

src/libraries/libmetget/src/libmetget/build/output/outputgrid.py
@@ -205,8 +205,16 @@ class OutputGrid:
         """
         from geopandas import points_from_xy
 
-        x = np.arange(self.__x_lower_left, self.__x_upper_right, self.__x_resolution)
-        y = np.arange(self.__y_lower_left, self.__y_upper_right, self.__y_resolution)
+        x = np.arange(
+            self.__x_lower_left,
+            self.__x_upper_right + self.__x_resolution,
+            self.__x_resolution,
+        )
+        y = np.arange(
+            self.__y_lower_left,
+            self.__y_upper_right + self.__y_resolution,
+            self.__y_resolution,
+        )
         self.__x_points = x
         self.__y_points = y
         self.__grid_points = np.array(np.meshgrid(x, y))