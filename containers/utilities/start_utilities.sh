af15ff73fc15013c62e24c45d202dbd2ac6b56c3
Zach Cobell <zcobell@gmail.com>
2024-10-24 12:30:21 -0400

    Fixing typo

--- containers/utilities/start_utilities.sh
+++ containers/utilities/start_utilities.sh
@@ -8,7 +8,7 @@ if [[ $# -ne 1 ]] ; then
 fi
 
 namespace=$1
-pod_name=$(kubectl get pods -l=app=utilties -n $1 --no-headers | cut -d" " -f1)
+pod_name=$(kubectl get pods -l=app=utilities -n $1 --no-headers | cut -d" " -f1)
 
 echo "[INFO]: Starting utilities in namespace: $1 (pod: $pod_name)"
 kubectl -n $1 exec -it $pod_name -- /bin/bash
