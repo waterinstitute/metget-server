ec75adc3beed38eeb45d80540486d2081c9c71d2
Zach Cobell <zcobell@gmail.com>
2024-10-24 11:16:09 -0400

    Using ARM specific runners

---
+++ containers/utilities/start_utilities.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]] ; then
+    echo "[ERROR]: Not enough arguments supplied"
+    echo "Usage:"
+    echo "./start_utilities.sh [namespace]"
+    exit 1
+fi
+
+namespace=$1
+pod_name=$(kubectl get pods -l=app=utilties -n $1 --no-headers | cut -d" " -f1)
+
+echo "[INFO]: Starting utilities in namespace: $1 (pod: $pod_name)"
+kubectl -n $1 exec -it $pod_name -- /bin/bash
