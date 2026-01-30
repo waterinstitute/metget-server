3bec9a346eb699829638d8e9f5b5995278e977da
Zach Cobell <zcobell@gmail.com>
2026-01-30 09:15:24 -0500

    Updating copyright year

--- LICENSE.md
+++ LICENSE.md
@@ -1,7 +1,7 @@
 The MIT License (MIT)
 =====================
 
-Copyright © 2023 The Water Institute
+Copyright © 2026 The Water Institute
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
--- src/executables/download/src/metget_download/download.py
+++ src/executables/download/src/metget_download/download.py
@@ -2,7 +2,7 @@
 ###################################################################################################
 # MIT License
 #
-# Copyright (c) 2023 The Water Institute
+# Copyright (c) 2026 The Water Institute
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
